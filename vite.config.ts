6bbcdd3c3ccd9e8df584847eb5c5dea9de9c6fe0
Chuen Kai <chuenkaiong@gmail.com>
2021-08-16 12:21:23 +0800

    config for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,12 @@ import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()]
+  plugins: [preact()],
+  base: "/classpart-client/",
+  server: {
+    fs: {
+      allow: [""],
+      strict: true
+    }
+  }
 })
